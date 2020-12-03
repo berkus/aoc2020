c9ba83d4015b3de7cf7ab58c141aa13dd86ab2ac
Berkus Decker <berkus+github@metta.systems>
2020-12-03 22:53:01 +0200

    Day 3, part 2

--- src/day3.rs
+++ src/day3.rs
@@ -17,9 +17,44 @@ pub fn input_generator_1(input: &str) -> i32 {
     total_trees
 }
 
+#[aoc_generator(day3, part2)]
+pub fn input_generator_2(input: &str) -> [i64; 5] {
+    let mut sums = [0i64; 5]; // right1_down1, right3_down1, right5_down1, right7_down1, right1_down2
+    let mut poss = [0usize; 5];
+    let mut flip = true;
+    let incr = [1usize, 3, 5, 7, 1];
+    input
+        .lines()
+        .map(|l| {
+            let modulo = l.len();
+            for idx in 0..=4 {
+                if idx == 4 {
+                    if !flip {
+                        flip = true;
+                        continue;
+                    } else {
+                        flip = false;
+                    }
+                }
+                if l.chars().nth(poss[idx]) == Some('#') {
+                    sums[idx] += 1;
+                }
+                poss[idx] = (poss[idx] + incr[idx]) % modulo;
+            }
+        })
+        .for_each(drop);
+    println!("{:?}", sums);
+    sums
+}
+
 #[aoc(day3, part1)]
 pub fn part1(input: &i32) -> i32 {
     *input
+}
+
+#[aoc(day3, part2)]
+pub fn part2(input: &[i64]) -> i64 {
+    input[0] * input[1] * input[2] * input[3] * input[4]
 }
 
 #[cfg(test)]
